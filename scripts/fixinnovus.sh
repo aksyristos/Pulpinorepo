a54483fd871f798f02b9c6d4a9d4d06d65bd2256
Giorgos Kollias <114400102+aksyristos@users.noreply.github.com>
2025-04-09 14:58:05 +0200

    Synthesis

---
+++ scripts/fixinnovus.sh
@@ -0,0 +1,5 @@
+ln -s /usr/local-eit/cad2/cmpstm/stm065v536/CLOCK65LPLVT_3.1/libs/CLOCK65LPLVT_wc_1.10V_125C_10y.lib CLOCK65LPLVT_wc_1.10V_125C_10y.lib
+ln -s /usr/local-eit/cad2/cmpstm/stm065v536/CORE65LPLVT_5.1/libs/CORE65LPLVT_wc_1.10V_125C_10y.lib CORE65LPLVT_wc_1.10V_125C_10y.lib
+ln -s /usr/local-eit/cad2/cmpstm/oldmems/mem2011/SPHD110420-48158@1.0/libs/SPHD110420_wc_1.10V_125C_10y.lib SPHD110420_wc_1.10V_125C_10y.lib
+ln -s /usr/local-eit/cad2/cmpstm/dicp18/lu_pads_65nm/Pads_Oct2012.lib Pads_Oct2012.lib
+
