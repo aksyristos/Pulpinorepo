eac089ffcfb11fa4f71b79899b0014e3d25454c9
BigLuxhi <xhuliocene207@outlook.com>
2025-04-28 14:15:18 +0200

    Final C code used to measure the performance

--- secondproject.c
+++ secondproject.c
@@ -1,5 +1,6 @@
 //Check Instruction Register waveform in simulation to measure the computation time
 //Time of computation in clock cycles: 550
+
     #include <stdio.h>
     volatile int  O[4][4];
 
